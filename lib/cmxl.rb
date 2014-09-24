8b75678633dae81d46f49f65cd244afa515e2c06
Michael Bumann <michael@railslove.com>
2014-09-24 11:56:47 +0200

    remove autoload
    the field parsers automatically register them self at the parser
    registry (Cmxl::Field.parsers). but for that they need to be loaded
    thus we load all the files and parsers when cmxl is required.

--- lib/cmxl.rb
+++ lib/cmxl.rb
@@ -1,21 +1,10 @@
 require "cmxl/version"
 
 require 'cmxl/field'
+require 'cmxl/statement'
+require 'cmxl/transaction'
 Dir[File.join(File.dirname(__FILE__), 'cmxl/fields', '*.rb')].each { |f| require f; }
 module Cmxl
-  module Fields
-    autoload :AccountBalance, 'cmxl/fields/account_balance'
-    autoload :AccountIdentification, 'cmxl/fields/account_identification'
-    autoload :AvailableBalance, 'cmxl/fields/available_balance'
-    autoload :ClosingBalance, 'cmxl/fields/closing_balance'
-    autoload :Reference, 'cmxl/fields/reference'
-    autoload :StatementDetails, 'cmxl/fields/statement_details'
-    autoload :StatementLine, 'cmxl/fields/statement_line'
-    autoload :StatementNumber, 'cmxl/fields/statement_number'
-  end
-  autoload :Field, 'cmxl/field'
-  autoload :Statement, 'cmxl/statement'
-  autoload :Transaction, 'cmxl/transaction'
 
   def self.parse(data, options={})
     options[:universal_newline] ||= true
