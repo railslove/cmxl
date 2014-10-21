74f2b86c239dc9a5a8c5463e6e60dae60a3de6c6
Michael Bumann <michael@railslove.com>
2014-10-21 18:39:47 +0200

    add codeclimate coverage report to the pretty much very first line

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'cmxl'
-
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+
+require 'cmxl'
+
 
 puts File.join(File.dirname(__FILE__), "support/**/*.rb").to_s
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
