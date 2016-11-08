daa77023745124d3b5b72328067d6bc3bfcfb054
Lars Brillert <lars@railslove.com>
2016-11-08 14:13:47 +0100

    fix codeclimate setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 require 'cmxl'
 
