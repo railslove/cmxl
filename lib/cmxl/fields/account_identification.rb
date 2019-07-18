82e32f3d516b519a5b1860118fefa8bf6d6f8150
Lars Brillert <lars@railslove.com>
2019-07-18 07:58:16 +0200

    handling account identification in a sligthly different variant, the bank code is shorter and the currency is separated with an additional `/`. this was happening for an austian bank

--- lib/cmxl/fields/account_identification.rb
+++ lib/cmxl/fields/account_identification.rb
@@ -2,7 +2,7 @@ module Cmxl
   module Fields
     class AccountIdentification < Field
       self.tag = 25
-      self.parser = /(?<bank_code>\w{8,11})\/(?<account_number>\d{0,23})(?<currency>[A-Z]{3})?|(?<country>[a-zA-Z]{2})(?<ban>\d{11,36})/i
+      self.parser = /(?<bank_code>\w{7,11})\/(?<account_number>\d{0,23})\/?(?<currency>[A-Z]{3})?|(?<country>[a-zA-Z]{2})(?<ban>\d{11,36})/i
 
       def iban
         "#{country}#{ban}"
