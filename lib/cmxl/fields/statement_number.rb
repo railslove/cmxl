13d97a313234351b3f6c66e440b4a6dc88d5c1bc
Markus Andree <andree@s-e-j.de>
2021-04-01 09:47:00 +0200

    Parse statement numbers  correctly based on specs

--- lib/cmxl/fields/statement_number.rb
+++ lib/cmxl/fields/statement_number.rb
@@ -2,7 +2,7 @@ module Cmxl
   module Fields
     class StatementNumber < Field
       self.tag = 28
-      self.parser = /(?<statement_number>\d{5})\/(?<sequence_number>\d{3,5})/
+      self.parser = /(?<statement_number>\d{1,5})(?:\/(?<sequence_number>\d{1,5}))?/
     end
   end
 end
